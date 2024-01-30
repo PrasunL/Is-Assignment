138ece4b0ee47cc3781f697f68ce22520a0b3750
Prasun Lamichhane <83114352+PrasunL@users.noreply.github.com>
2024-01-30 21:45:47 +0545

    added comments
    life is not daijobu

--- coursework/js/script.js
+++ coursework/js/script.js
@@ -1,8 +1,8 @@
-var images = ["img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg", "img5.jpg"];
+var images = ["img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg", "img5.jpg"];//declares array of 5 images
 var x = 0;
 var imgs = document.getElementById("imgs");
 
-setInterval(slider, 2000);
+setInterval(slider, 2000);//calls function slider every 2 seconds
 
 function slider() {
   if (x < images.length) {
@@ -11,14 +11,15 @@ function slider() {
     x = 1;
   }
   imgs.innerHTML = "<img src=./pictures/sliderImages/" + images[x - 1] + ">";
-}
+}//changes image accordingly
 
 function validateForm() {
-  var fname = document.getElementById("fname").value;
-  var lname = document.getElementById("lname").value;
-  var email = document.getElementById("email").value;
-  var gender = document.querySelector('input[name="gender"]:checked');
-  var feedback = document.getElementById("feedback").value;
+  var fname = document.getElementById("fname").value; //calls the values of first name
+  var lname = document.getElementById("lname").value;//calls the values of last name
+  var email = document.getElementById("email").value;//calls the values of email
+  var gender = document.querySelector('input[name="gender"]:checked');//calls the value of gender
+  var feedback = document.getElementById("feedback").value;//calls the values of feedback message
+  
 
   if (
     fname === "" ||
@@ -34,7 +35,7 @@ function validateForm() {
     alert("Thank you for your feedback.");
   }
   return false;
-}
+}//checks if the form is valid to send or not
 
 function togglePopup(pdfFile) {
   var popup = document.getElementById("pdfPopup");
@@ -43,11 +44,11 @@ function togglePopup(pdfFile) {
 
   pdfViewer.src = pdfPath;
   popup.style.display = "block";
-}
+}//displays the cvs of members
 
 function closePopup() {
   var popup = document.getElementById("pdfPopup");
   var pdfViewer = document.getElementById("pdfViewer");
   pdfViewer.src = "";
   popup.style.display = "none";
-}
+}//removes the cvs of members
